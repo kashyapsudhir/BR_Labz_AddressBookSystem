856f93873e68c78e1d373632e4e97a0f60f817a6
kashyapsudhir <kashyapsudhir1996@gmail.com>
2023-05-18 12:56:16 +0530

    [create contact details  ]

--- AddressBookSystem/AddressBookSystem/Program.cs
+++ AddressBookSystem/AddressBookSystem/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Transactions;
 
 namespace AddressBookSystem
 {
@@ -7,6 +8,37 @@ namespace AddressBookSystem
         public static void Main(string[] args) 
         {
             Console.WriteLine("Welcome to Address Book Program in AddressBookMain class on main branch");
+
+            ContactClass contact = new ContactClass();
+
+            Console.WriteLine("Enter First Name");
+            contact.FirstName = Console.ReadLine();
+
+            Console.WriteLine("Enter Last Name");
+            contact.LastName = Console.ReadLine();
+
+            Console.WriteLine("Enter your Address");
+            contact.Address = Console.ReadLine();
+
+
+            Console.WriteLine("Enter your City");
+            contact.City = Console.ReadLine();
+
+            Console.WriteLine("Enter your State");
+            contact.State = Console.ReadLine();
+
+            Console.WriteLine("Enter your ZIP");
+            contact.Zip = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter your Phone Number");
+            contact.PhoneNumber = Console.ReadLine();
+
+            Console.WriteLine("Enter your Email");
+            contact.Email = Console.ReadLine();
+
+
+
+
         }
     }
 }
